302aea57dd65900623547ea94a56f62734a01d4e
Stanislava <stanislavasevova2013@u.northwestern.edu>
2020-09-18 17:26:32 +0200

    modify script to run NN HPO on GRID

--- nnKerasTrain.py
+++ nnKerasTrain.py
@@ -1,56 +1,55 @@
-import numpy as np
-import pandas as pd
-#keras
-from tensorflow.keras import Model, Sequential
-from tensorflow.keras.layers import Dropout, RMSprop, Dense
-#from tensorflow.keras.constraints import maxnorm
-#from tensorflow.keras.optimizers import Adam
-#sklearn_pandas
-
+import argparse
 # ray tune
 from ray import tune
-
-class CreateModel(Sequential):
-    def __init__(self, 
-                 n1=160, n2=135, n3=8, n4=8, 
-                 init_mode='lecun_uniform',
-                 dropout_rate=0.0):
-        super(CreateModel, self).__init__()
-        self.dr = Dropout(dropout_rate)
-        self.l1 = Dense(n1, activation="relu",kernel_initializer=init_mode)
-        self.l2 = Dense(n2, activation="relu",kernel_initializer=init_mode)
-        self.l3 = Dense(n3, activation="relu",kernel_initializer=init_mode)
-        self.l4 = Dense(n4, activation="relu",kernel_initializer=init_mode)
-        self.lf = Dense(1, activation="sigmoid",kernel_initializer=init_mode)
-        
-    def call(self, x):
-        x = self.l1(x)
-        x = self.dr(x)
-        x = self.l2(x)
-        x = self.dr(x)
-        x = self.l3(x)
-        x = self.dr(x)
-        x = self.l4(x)
-        x = self.dr(x)
-        return self.lf(x)
-  
+# Variables of interest
+varw = ['met_tight_tst_et','met_tight_tst_phi','mT','ph_pt','dphi_mety_ll','AbsPt','Ptll','mllg','lep1pt','lep2pt','mll','metsig_tst','Ptllg','dphi_met_ph','w']
 
 class DarkPhoton_NN(tune.Trainable):
+
     def _setup(self, config):
+        self.n1 = config.get("n1", 160)
+        self.n2 = config.get("n2", 135)
+        self.n3 = config.get("n3", 8)
+        self.n4 = config.get("n4", 8)
+        self.dr = config.get("dropout_rate", 0.0)
+        self.init_mode = config.get("init_mode","lecun_uniform")
+        self.lr = config.get("lr",1e-4)
+        self.batchsize = config.get('batchsize', 64)
+
         import tensorflow as tf
+
+
+
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(self.n1, activation="relu",kernel_initializer=self.init_mode, input_shape=(len(varw)-1,)))
+        model.add(tf.keras.layers.Dropout(self.dr))
+        model.add(tf.keras.layers.Dense(self.n2, activation="relu",kernel_initializer=self.init_mode))
+        model.add(tf.keras.layers.Dropout(self.dr))
+        model.add(tf.keras.layers.Dense(self.n3, activation="relu",kernel_initializer=self.init_mode))
+        model.add(tf.keras.layers.Dropout(self.dr))        
+        model.add(tf.keras.layers.Dense(self.n4, activation="relu",kernel_initializer=self.init_mode))
+        model.add(tf.keras.layers.Dropout(self.dr))        
+        model.add(tf.keras.layers.Dense(1, activation="sigmoid",kernel_initializer=self.init_mode))
+        model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate = self.lr), 
+                      loss = tf.keras.losses.BinaryCrossentropy(),
+                      metrics=['AUC'])
+        print(model.summary())
+        self.model = model
+
+    def _train(self):                
+
+        import pandas as pd
+        import sklearn.model_selection as model_selection
         import sklearn as skl
         import sklearn_pandas as skp
-
-        all_data = pd.read_csv("all_data")
+        
+        all_data = pd.read_csv("/afs/cern.ch/work/s/ssevova/public/dark-photon-atlas/zhdarkphotonml/all_data")
         all_data = all_data[all_data['w']>0]
 
-        # Variables of interest
-        varw = ['met_tight_tst_et','met_tight_tst_phi','mT','ph_pt','dphi_mety_ll','AbsPt','Ptll','mllg','lep1pt','lep2pt','mll','metsig_tst','Ptllg','dphi_met_ph','w']
-            
         # Load the data & split by train/test
         X = all_data
         y = all_data['event']
-        X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=0)
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y,test_size=0.3, random_state=0)
         X_train = X_train[varw]
         X_test = X_test[varw]
         cols = X_train.columns
@@ -72,77 +71,20 @@ class DarkPhoton_NN(tune.Trainable):
         X_train = X_scaled_train[varw]
         X_test = X_scaled_test[varw]
         
-        # Add a channels dimension
-        X_train = X_train[..., tf.newaxis]
-        X_test = X_test[..., tf.newaxis]
-
-
-        self.train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-        self.train_ds = self.train_ds.shuffle(10000).batch(
-            config.get("batchsize", 32))
-
-        self.test_ds = tf.data.Dataset.from_tensor_slices((X_test,
-                                                           y_test)).batch(32) 
-        self.model = CreateModel(n1=config.get("n1",160),
-                                 n2=config.get("n2",135),
-                                 n3=config.get("n3",8),
-                                 n4=config.get("n4",8),
-                                 dropout_rate=config.get("dropout_rate",0.0),
-                                 init_mode=config.get("init_mode","lecun_uniform"))
-                             
-        self.loss_object = tf.keras.losses.BinaryCrossentropy()
-        self.optimizer = tf.keras.optimizers.Adam(
-            learning_rate=config.get("lr",1e-4),
-            beta_1=config.get("beta_1",0.9))
-        self.train_loss = kf.keras.metrics.AUC(name="train_loss")
-        self.train_accuracy = kf.keras.metrics.BinaryAccuracy(name="train_accuracy")
-        self.test_loss = kf.keras.metrics.AUC(name="test_loss")
-        self.test_accuracy = kf.keras.metrics.BinaryAccuracy(name="test_accuracy")
-
-        @tf.function
-        def train_step(images, labels):
-            with tf.GradientTape() as tape:
-                predictions = self.model(images)
-                loss = self.loss_object(labels, predictions)
-            gradients = tape.gradient(loss, self.model.trainable_variables)
-            self.optimizer.apply_gradients(
-                zip(gradients, self.model.trainable_variables))
-
-            self.train_loss(loss)
-            self.train_accuracy(labels, predictions)
-
-        @tf.function
-        def test_step(images, labels):
-            predictions = self.model(images)
-            t_loss = self.loss_object(labels, predictions)
-
-            self.test_loss(t_loss)
-            self.test_accuracy(labels, predictions)
-
-        self.tf_train_step = train_step
-        self.tf_test_step = test_step
-
-    def _train(self):
-        self.train_loss.reset_states()
-        self.train_accuracy.reset_states()
-        self.test_loss.reset_states()
-        self.test_accuracy.reset_states()
-
-        for idx, (images, labels) in enumerate(self.train_ds):
-            if idx > MAX_TRAIN_BATCH:  # This is optional and can be removed.
-                break
-            self.tf_train_step(images, labels)
-
-        for test_images, test_labels in self.test_ds:
-            self.tf_test_step(test_images, test_labels)
+        history = self.model.fit(X_train, y_train, epochs=50,
+                                 batch_size=self.batchsize,
+                                 validation_data=(X_test, y_test))
+        
+        train_loss, train_acc = self.model.evaluate(X_train, y_train, verbose=2)
+        test_loss, test_acc = self.model.evaluate(X_test, y_test, verbose=2)
 
         # It is important to return tf.Tensors as numpy objects.
         return {
             "epoch": self.iteration,
-            "loss": self.train_loss.result().numpy(),
-            "accuracy": self.train_accuracy.result().numpy() * 100,
-            "test_loss": self.test_loss.result().numpy(),
-            "mean_accuracy": self.test_accuracy.result().numpy() * 100
+            "loss": train_loss,
+            "accuracy": train_acc,
+            "test_loss": test_loss,
+            "test_accuracy": test_acc
         }
 
 
