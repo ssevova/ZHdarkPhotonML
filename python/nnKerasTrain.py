2c31b692e5e295d644724985c803b39e21a64c3e
Stanislava <stanislavasevova2013@u.northwestern.edu>
2020-09-21 10:23:04 +0200

    script for n_layer search

--- python/nnKerasTrain.py
+++ python/nnKerasTrain.py
@@ -1,5 +1,4 @@
 import argparse
-
 # ray tune
 from ray import tune
 # Variables of interest
@@ -8,26 +7,35 @@ varw = ['met_tight_tst_et','met_tight_tst_phi','mT','ph_pt','dphi_mety_ll','AbsP
 class DarkPhoton_NN(tune.Trainable):
 
     def _setup(self, config):
+        self.epochs = config.get("epochs",50)
+        self.dense_layers = config.get("dense_layers",2)
+        self.top_layer_neurons = config.get("top_layer_neurons",160)
+        self.bot_layer_neurons = config.get("bot_layer_neurons",160)
+        self.dense_activation = config.get("dense_activation","relu")
+        '''
         self.n1 = config.get("n1", 160)
         self.n2 = config.get("n2", 135)
         self.n3 = config.get("n3", 8)
         self.n4 = config.get("n4", 8)
+        '''
         self.dr = config.get("dropout_rate", 0.0)
         self.init_mode = config.get("init_mode","lecun_uniform")
         self.lr = config.get("lr",1e-4)
         self.batchsize = config.get('batchsize', 64)
-
+        self.run_mode = config.get("run_mode","local")
         import tensorflow as tf
 
         model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(self.n1, activation="relu",kernel_initializer=self.init_mode, input_shape=(len(varw)-1,)))
-        model.add(tf.keras.layers.Dropout(self.dr))
-        model.add(tf.keras.layers.Dense(self.n2, activation="relu",kernel_initializer=self.init_mode))
-        model.add(tf.keras.layers.Dropout(self.dr))
-        model.add(tf.keras.layers.Dense(self.n3, activation="relu",kernel_initializer=self.init_mode))
-        model.add(tf.keras.layers.Dropout(self.dr))        
-        model.add(tf.keras.layers.Dense(self.n4, activation="relu",kernel_initializer=self.init_mode))
-        model.add(tf.keras.layers.Dropout(self.dr))        
+        for layer in range(self.dense_layers):
+            if layer == 0: 
+                model.add(tf.keras.layers.Dense(self.top_layer_neurons, activation=self.dense_activation,kernel_initializer=self.init_mode, input_shape=(len(varw)-1,)))
+            else: 
+                if layer < self.dense_layers/2:
+                    model.add(tf.keras.layers.Dense(self.top_layer_neurons, activation=self.dense_activation, kernel_initializer=self.init_mode))
+                else: 
+                    model.add(tf.keras.layers.Dense(self.bot_layer_neurons, activation=self.dense_activation, kernel_initializer=self.init_mode))
+            model.add(tf.keras.layers.Dropout(self.dr))
+            
         model.add(tf.keras.layers.Dense(1, activation="sigmoid",kernel_initializer=self.init_mode))
         model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate = self.lr), 
                       loss = tf.keras.losses.BinaryCrossentropy(),
@@ -42,13 +50,18 @@ class DarkPhoton_NN(tune.Trainable):
         import sklearn_pandas as skp
         import hpogrid
         import glob
+        
+        if self.run_mode == "grid":
+            data_directory = hpogrid.get_datadir()
+            print("inDS dir: {}".format(data_directory))
+            data_files = glob.glob(data_directory+"*.csv")
+            print("--> inDS file: {}".format(data_files))
+            for f in data_files:
+                all_data = pd.read_csv(f)
+        elif self.run_mode == "local":
+            all_data = pd.read_csv('/afs/cern.ch/work/s/ssevova/public/dark-photon-atlas/plotting/trees/v08/tight-and-ph-skim/mc16d/dataLists/all_data')
 
-        data_directory = hpogrid.get_datadir()
-        print("inDS dir: {}".format(data_directory))
-        data_files = glob.glob(data_directory+"*.csv")
-        print("--> inDS file: {}".format(data_files))
-        for f in data_files:
-            all_data = pd.read_csv(f)
+        # Remove negative weights for training
         all_data = all_data[all_data['w']>0]
 
         # Load the data & split by train/test
@@ -76,7 +89,7 @@ class DarkPhoton_NN(tune.Trainable):
         X_train = X_scaled_train[varw]
         X_test = X_scaled_test[varw]
         
-        history = self.model.fit(X_train, y_train, epochs=50,
+        history = self.model.fit(X_train, y_train, epochs=self.epochs,
                                  batch_size=self.batchsize,
                                  validation_data=(X_test, y_test))
         
