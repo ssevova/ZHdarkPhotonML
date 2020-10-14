6ff17b9303b2768761e8b55764d993255f51e9ef
Stanislava <stanislavasevova2013@u.northwestern.edu>
2020-10-14 19:49:17 +0200

    fix hpo bdt train script

--- python/bdtTrain.py
+++ python/bdtTrain.py
@@ -10,7 +10,7 @@ from ray import tune
 class DarkPhoton_BDT(tune.Trainable):
 
     def setup(self, config):
-        self.n_estimators = config.get("n_estimators",200)
+        self.n_estimators = int(config.get("n_estimators",200))
         self.max_depth = config.get("max_depth",3)
         self.subsample = config.get("subsample",0.5)
         self.max_features = config.get("max_features",0.5)
@@ -20,10 +20,18 @@ class DarkPhoton_BDT(tune.Trainable):
         self.warm_start = config.get("warm_start",False)
 
         self.varw = config.get("varslist",['met_tight_tst_et'])
-        #['met_tight_tst_et','met_tight_tst_phi','mT','ph_pt','dphi_mety_ll','AbsPt','Ptll','mllg','lep1pt','lep2pt','mll','metsig_tst','Ptllg','dphi_met_ph','w']
+        self.run_mode = config.get("run_mode","local")
+
         from sklearn.ensemble import GradientBoostingClassifier
-        model = GradientBoostingClassifier(n_estimators = self.n_estimators,max_depth = self.max_depth,subsample=self.subsample,max_features=self.max_features,learning_rate=self.learning_rate,min_samples_split = self.min_samples_split,min_samples_leaf=self.min_samples_leaf,warm_start=self.warm_start)
-        print(model.summary())
+        model = GradientBoostingClassifier(n_estimators = self.n_estimators,
+                                           max_depth = self.max_depth,
+                                           subsample=self.subsample,
+                                           max_features=self.max_features,
+                                           learning_rate=self.learning_rate,
+                                           min_samples_split = self.min_samples_split,
+                                           min_samples_leaf=self.min_samples_leaf,
+                                           warm_start=self.warm_start)
+        print(model.get_params())
         self.model = model
 
     def step(self):                
@@ -42,41 +50,38 @@ class DarkPhoton_BDT(tune.Trainable):
             for f in data_files:
                 all_data = pd.read_csv(f)
         elif self.run_mode == "local":
-            all_data = pd.read_csv('/afs/cern.ch/work/s/ssevova/public/dark-photon-atlas/plotting/trees/v08/tight-and-ph-skim/mc16d/dataLists/all_data')
+            all_data = pd.read_csv('/afs/cern.ch/work/s/ssevova/public/dark-photon-atlas/zhdarkphotonml/samples/v09/mc16d_v09_samples.csv')
 
         # Remove negative weights for training
         all_data = all_data[all_data['w']>0]
 
         # Load the data & split by train/test
         X = all_data
-        y = all_data['event']
-        X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y,test_size=0.3, random_state=0)
-        X_train = X_train[self.varw]
-        X_test = X_test[self.varw]
+        y = all_data['event'].astype(int)
+        X_train, X_test, y_train, y_test = skl.model_selection.train_test_split(X, y,test_size=0.3, random_state=0)
         wtest = X_test['w']
         wtrain = X_train['w']
 
-        # Deal with weights
         self.varw.remove("w")
-        X_train = X_scaled_train[self.varw]
-        X_test = X_scaled_test[self.varw]
-        
+        X_train = X_train[self.varw]
+        X_test = X_test[self.varw]
+
         history = self.model.fit(X_train, y_train, sample_weight=wtrain)
         # Could also add recall, precision etc
-        train_acc = self.model.score(X_train,y_train) 
-        test_acc = self.model.score(X_test, y_test)
-        
-        probs = self.model.predict(X_test)
-        predictions = self.model.predict_classes(X_test)
-        fpr,tpr,threshold = skl.metrics.roc_curve(y_test,probs,sample_weight=wtest_unscaled)
+        train_acc = history.score(X_train,y_train) 
+        test_acc = history.score(X_test, y_test)
+        predictions = history.predict(X_test)        
+        probs = history.predict_proba(X_test)
+        y_score = history.decision_function(X_test)
+
+        fpr,tpr,threshold = skl.metrics.roc_curve(y_test,y_score,sample_weight=wtest)
         auc = skl.metrics.auc(fpr,tpr) 
         
         return {
             #"epoch": self.iteration,
             #"loss": train_loss,
-            "accuracy": train_acc,
+            "train_accuracy": train_acc,
             "auc": auc,
-            "test_loss": test_loss,
             "test_accuracy": test_acc
         }
   
