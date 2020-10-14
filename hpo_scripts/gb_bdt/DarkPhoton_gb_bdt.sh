d0ce727fe8983993351ba7a074b25686f466b188
Stanislava <stanislavasevova2013@u.northwestern.edu>
2020-10-14 19:49:51 +0200

    fix hpo config script for bdt

--- hpo_scripts/gb_bdt/DarkPhoton_gb_bdt.sh
+++ hpo_scripts/gb_bdt/DarkPhoton_gb_bdt.sh
@@ -1,7 +1,6 @@
-hpogrid model_config recreate DarkPhoton_gb_bdt --script bdtTrain.py --model DarkPhoton_gb_bdt --param '{"run_mode":"grid"}'
+hpogrid model_config recreate DarkPhoton_gb_bdt --script bdtTrain.py --model DarkPhoton_BDT --param '{"run_mode":"grid", "varslist":["met_tight_tst_et","met_tight_tst_phi","mT","ph_pt","dphi_mety_ll","AbsPt","Ptll","mllg","lep1pt","lep2pt","mll","metsig_tst","Ptllg","dphi_met_ph","w"]}'
 hpogrid search_space recreate bdt_space '{"n_estimators":{"method":"uniform","dimension":{"low":200,"high":900}},"max_depth":{"method":"categorical","dimension":{"categories":[2,3,4,5,6,7,8]}},"sub_sample":{"method":"uniform","dimension":{"low":0.5,"high":0.95}},"max_features":{"method":"categorical","dimension":{"categories":[1,2,3,4,5,6,7,8,9,10]}},"learning_rate":{"method":"loguniform","dimension":{"low":1e-04,"high":0.1}},"min_samples_split":{"method":"categorical","dimension":{"categories":[2,4,6,8,10,20,40,60,100]}},"min_samples_leaf":{"method":"categorical","dimension":{"categories":[1,3,5,7,9]}}}'
-
-hpogrid hpo_config recreate nevergrad_max_auc --metric auc --mode max --num_trials 1 --algorithm nevergrad
+hpogrid hpo_config recreate nevergrad_max_auc --metric auc --mode max --num_trials 5 --algorithm nevergrad
 # What is this?
-hpogrid grid_config recreate grid_run --inDS user.ssevova:ZHyyD_v09_mc16a_ML --outDS user.ehofgard.BDTtest
-hpogrid project recreate DarkPhoton_gb_bdt --model_config DarkPhoton_gb_bdt --search_space bdt_space --hpo_config nevergrad_max_auc --grid_config grid_run --scripts_path /afs/cern.ch/user/e/ehofgard/public/ZHdarkPhotonML/source/python
+hpogrid grid_config recreate grid_run_v09_mc16a --inDS user.ssevova:ZHyyD_v09_mc16a_ML 
+hpogrid project recreate DarkPhoton_gb_bdt --model_config DarkPhoton_gb_bdt --search_space bdt_space --hpo_config nevergrad_max_auc --grid_config grid_run_v09_mc16a --scripts_path /afs/cern.ch/work/s/ssevova/public/dark-photon-atlas/zhdarkphotonml/python
