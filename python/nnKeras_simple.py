6c324ce2d0d4b5affdc540a45cac4e7c03b66766
Elyssa Frances Hofgard <ehofgard@lxplus7108.cern.ch>
2020-10-16 20:20:00 +0200

    plotting correlations

--- python/nnKeras_simple.py
+++ python/nnKeras_simple.py
@@ -17,40 +17,34 @@ import uproot as up
 import uproot_methods
 import pandas as pd
 import matplotlib.pyplot as plt
-# sklearn
-from sklearn.model_selection import GridSearchCV
+
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.metrics import classification_report
-from sklearn import preprocessing
 from keras.layers import Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
 from keras.constraints import maxnorm
 from keras.optimizers import Adam
 from keras.optimizers import RMSprop
-from sklearn_pandas import DataFrameMapper
-from sklearn.preprocessing import StandardScaler
+
+# sklearn
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import RandomizedSearchCV
-#keras
-from keras.layers import Dropout
-from keras.constraints import maxnorm
-from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import cross_val_score
+
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+
 #sklearn_pandas
 from sklearn_pandas import DataFrameMapper
+
 # Function to create model, required for KerasClassifier
 # Command line arguments
 ######################## 
@@ -61,33 +55,103 @@ def getArgumentParser():
                         '--infile',
                         dest='infile',
                         help='Input CSV file',
-                        default = '/afs/cern.ch/user/e/ehofgard/public/data/all_data')
+                        default = '/afs/cern.ch/work/s/ssevova/public/dark-photon-atlas/zhdarkphotonml/samples/v09/mc16d_v09_samples.csv')
     parser.add_argument('-o',
                         '--output',
                         dest='outdir',
                         help='Output directory for plots, selection lists, etc',
                         default='outdir')
+    parser.add_argument('--plotInputs',action='store_true', help='Plot scaled train & test inputs')
+    parser.add_argument('--plotOutputs',action='store_true', help='Plot scaled test outputs for given probability range')
+    parser.add_argument('--plotCorrelationInputs',action='store_true',help='Plot correlation matrices for scaled train & test inputs')
+    parser.add_argument('--plotCorrelationOutPuts',action='store_true',help='Plot correlation matrices for scaled train & test inputs for given probability range')
 
     return parser
 ##############################################################################
 def create_model(input_dim):
     # create model
     model = Sequential()
-    model.add(Dense(180,activation='relu',input_dim = input_dim,kernel_initializer='lecun_uniform',kernel_constraint=maxnorm(5)))
-    #model.add(Dropout(0.1))
-    model.add(Dense(130, activation='relu',kernel_initializer='lecun_uniform',kernel_constraint=maxnorm(5)))
-    #,kernel_constraint=maxnorm(3)
-    #model.add(Dropout(0.1))
-    model.add(Dense(105,activation='relu',kernel_initializer='lecun_uniform',kernel_constraint=maxnorm(5)))
-    #,kernel_constraint=maxnorm(3)))
-    #model.add(Dropout(0.1))
-    model.add(Dense(25,activation='relu',kernel_initializer='lecun_uniform',kernel_constraint=maxnorm(5)))
-    #model.add(Dropout(0.1))
-    optimizer = RMSprop(learning_rate=0.01)
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(256,activation='relu',input_dim = input_dim,kernel_initializer='glorot_normal'))
+    model.add(Dropout(0.813755))
+    model.add(Dense(256, activation='relu',kernel_initializer='glorot_normal'))
+    model.add(Dropout(0.813755))
+    model.add(Dense(64,activation='relu',kernel_initializer='glorot_normal'))
+    model.add(Dropout(0.813755))
+    model.add(Dense(1, activation='sigmoid',kernel_initializer='glorot_normal'))
+
+    optimizer = RMSprop(learning_rate=0.001720)
+
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy','AUC','Recall'])
     return model
+
+def plot_inputs(X_scaled_train, X_scaled_test, y_train, y_test,varw):
+    X_scaled_train['event'] = y_train
+    X_scaled_test['event'] = y_test
+    for var in varw:
+        if var=='w': continue
+        sig_scaled_train = np.array(X_scaled_train[X_scaled_train['event']==1][var])
+        bkg_scaled_train = np.array(X_scaled_train[X_scaled_train['event']==0][var])
+        sig_scaled_test = np.array(X_scaled_test[X_scaled_test['event']==1][var])
+        bkg_scaled_test = np.array(X_scaled_test[X_scaled_test['event']==0][var])
+        plt.clf()
+        plt.hist(sig_scaled_train, bins=50, range=(-1,1), histtype='step', color='Blue',label='Train Sig (scaled)')
+        plt.hist(bkg_scaled_train, bins=50, range=(-1,1), histtype='step', color='Red',label='Train Bkg (scaled)')
+        plt.hist(sig_scaled_test, bins=50, range=(-1,1), histtype='stepfilled', alpha=0.2, color='Blue',label='Test Sig (scaled)')
+        plt.hist(bkg_scaled_test, bins=50, range=(-1,1), histtype='stepfilled', alpha=0.2, color='Red',label='Test Bkg (scaled)')
+        plt.xlabel(var)
+        plt.ylabel('Events')
+        plt.legend()
+        plt.savefig('input_scaled_train_test_'+var+'.pdf')
+
+def correlations(data,data_type, **kwds):
+    """Calculate pairwise correlation between features.
+    
+    Extra arguments are passed on to DataFrame.corr()
+    """
+    # simply call df.corr() to get a table of
+    # correlation values if you do not need
+    # the fancy plotting
+    corrmat = data.corr(**kwds)
+
+    fig, ax1 = plt.subplots(ncols=1, figsize=(6,5))
+    
+    opts = {'cmap': plt.get_cmap("RdBu"),
+            'vmin': -1, 'vmax': +1}
+    heatmap1 = ax1.pcolor(corrmat, **opts)
+    plt.colorbar(heatmap1, ax=ax1)
+
+    ax1.set_title("Correlations "+data_type)
+
+    labels = corrmat.columns.values
+    for ax in (ax1,):
+        # shift location of ticks to center of the bins
+        ax.set_xticks(np.arange(len(labels))+0.5, minor=False)
+        ax.set_yticks(np.arange(len(labels))+0.5, minor=False)
+        ax.set_xticklabels(labels, minor=False, ha='right', rotation=70)
+        ax.set_yticklabels(labels, minor=False)
+        
+    plt.tight_layout()
+    plt.savefig("Correlations_"+data_type+".pdf")
+def plot_outputs(X_test, y_test, varw):
+    X_test['event'] = y_test
+    X_test_filt = X_test[(X_test['prob'] >= 0.2) & (X_test['prob'] <= 0.4)]
+    varw.remove('w')
+    sig_test_filt = np.array(X_test_filt[X_test_filt['event']==1])
+    bkg_test_filt = np.array(X_test_filt[X_test_filt['event']==0])
+    correlations(bkg_test_filt,'Bkg')
+    correlations(sig_test_filt,'Sig') 
+    for var in varw:
+        if var=='w': continue
+        sig_test_filt = np.array(X_test_filt[X_test_filt['event']==1][var])
+        bkg_test_filt = np.array(X_test_filt[X_test_filt['event']==0][var])
+        plt.clf()
+        plt.hist(sig_test_filt, bins=50, range=(-1,1), histtype='step', color='Blue',label='Test Sig (scaled)')
+        plt.hist(bkg_test_filt, bins=50, range=(-1,1), histtype='step', color='Red',label='Test Bkg (scaled)')
+        plt.xlabel(var)
+        plt.ylabel('Events')
+        plt.legend()
+        plt.savefig('output_scaled_cond_test_'+var+'.pdf')
 
 def plot_roc(fpr,tpr,roc_auc):
     plt.clf()
@@ -114,9 +178,9 @@ def plot_probs(y_test,probs,weight,model):
     weight_1 = weight[arr_true_1_indices]
     plt.hist(arr_pred_0, bins=40, label='Background', density=True, histtype='step')
     plt.hist(arr_pred_1, bins=40, label='Signal', density=True, histtype='step')
-    plt.xlabel('Probability of being Positive Class')
+    plt.xlabel('Output Probability')
     plt.legend()
-    plt.title('Output Test Probabilities')
+    #    plt.title('Output Test Probabilities')
     plt.savefig('output_prob_' + model + ".pdf")
 
 def plot_history(history):
@@ -166,7 +230,7 @@ def main():
     all_data = all_data[all_data['w']>0]
 
     # Variables of interest
-    varw = ['met_tight_tst_et','met_tight_tst_phi','mT','ph_pt','dphi_mety_ll','AbsPt','Ptll','mllg','lep1pt','lep2pt','mll','metsig_tst','Ptllg','dphi_met_ph','w']
+    varw = ['met_tight_tst_et','met_tight_tst_phi','mT','ph_pt','dphi_mety_ll','Ptll','lep1pt','lep2pt','mll','metsig_tst','w','AbsPt','dphi_met_ph','mllg','Ptllg']
     units = ['GeV','Radians','GeV','GeV','Radians','','GeV','GeV','GeV','GeV','GeV',r'$\sqrt{GeV}$','GeV','Radians']
     
     #varw = ['met_tight_tst_et','mT','dphi_mety_ll','w']
@@ -183,31 +247,52 @@ def main():
     itrain = X_train.index
     itest = X_test.index
     wtest_unscaled = X_test['w']
+    wtrain_unscaled = X_train['w']
     # Scaling 
-    mapper = DataFrameMapper([(cols,StandardScaler())])
+    mapper = DataFrameMapper([(cols,preprocessing.StandardScaler())])
     scaled_train = mapper.fit_transform(X_train.copy(),len(cols))
     scaled_test = mapper.fit_transform(X_test.copy(),len(cols))
     X_scaled_train = pd.DataFrame(scaled_train,index = itrain, columns=cols)
     X_scaled_test  = pd.DataFrame(scaled_test,index = itest,columns=cols)
-    wtest = X_scaled_test['w']
+    wtest  = X_scaled_test['w']
     wtrain = X_scaled_train['w']
+    if options.plotInputs:
+        print('==> Plotting scaled inputs...')
+        plot_inputs(X_scaled_train, X_scaled_test, y_train, y_test, varw)    
     # Deal with weights
     # Build and train the classifier model
     varw.remove("w")
     X_train = X_scaled_train[varw]
     X_test = X_scaled_test[varw]
-    model = create_model(len(varw))
-    history = model.fit(X_train,y_train,batch_size = 64,verbose = 0, epochs=100,validation_data=(X_test, y_test))
-    #, shuffle=True)#sample_weight=wtrain)
-    plot_history(history)
-    probs = model.predict(X_test)
-    predictions = model.predict_classes(X_test)
-    fpr,tpr,threshold = metrics.roc_curve(y_test,probs,sample_weight=wtest_unscaled)
-    auc = metrics.auc(fpr,tpr)
-    plot_roc(fpr,tpr,auc)
-    plot_probs(y_test,probs,wtest_unscaled,"nn")
 
+    print('==> Creating model from inputs...')
+    model = create_model(len(varw))
+    history = model.fit(X_train,y_train,batch_size = 1024,verbose = 0, epochs=100,validation_data=(X_test, y_test))
+    #, shuffle=True)#sample_weight=wtrain)
+    print('==> Plotting training model history...')
+    plot_history(history)
+
+    probs_test = model.predict(X_test)
+    probs_train = model.predict(X_train)
+    # Not sure if I need this here
+    
+    predictions = (model.predict(X_test) > 0.5).astype("int32")
+    X_test['prob']  = probs_test
+    X_train['prob'] = probs_train
+    
+    if options.plotOutputs:
+
+        plot_outputs(X_test, y_test, varw)
+
+    fpr,tpr,threshold = metrics.roc_curve(y_test,probs_test,sample_weight=wtest_unscaled)
+    auc = metrics.auc(fpr,tpr)
+
+    print('==> Plotting testing ROC curve & output probabilities...')
+    plot_roc(fpr,tpr,auc)
+    plot_probs(y_test, probs_test, wtest_unscaled,"NN_test")
+    plot_probs(y_train, probs_train, wtrain_unscaled,"NN_train")
     sys.stdout = open('model_out.txt','wt')
+
     '''
     print('Accuracy:')
     print(metrics.accuracy_score(y_test,predictions,sample_weight=wtest))
@@ -225,3 +310,4 @@ def main():
 if __name__ == '__main__':
     main()
            
+
